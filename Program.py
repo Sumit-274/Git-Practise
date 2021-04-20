28715c86520346298b19dab39b8a6547644a335d
Sumit-274 <82871085+Sumit-274@users.noreply.github.com>
2021-04-20 21:16:58 +0530

    Update Program.py
    I have just inserted the if __name__ == "__main__":

--- Program.py
+++ Program.py
@@ -3,7 +3,7 @@ from plyer import notification
 def Notification(title,message):
     notification.notify(title=title,message=message)
 
-
-inp1 = input("Enter the Heading\n")
-inp2 = input("Enter the Message\n")
-Notification(inp1,inp2)    
+if __name__ == "__main__":
+    inp1 = input("Enter the Heading\n")
+    inp2 = input("Enter the Message\n")
+    Notification(inp1,inp2)    
