c4960f60e0ea355dc6d089d297f63ef5bffee335
Sumit <sumitghotane274@gmail.com>
2021-04-21 11:09:53 +0530

    Time out Created

--- Program.py
+++ Program.py
@@ -1,9 +1,10 @@
 from plyer import notification
 
-def Notification(title,message):
-    notification.notify(title=title,message=message)
+def Notification(title,message,timeout):
+    notification.notify(title=title,message=message,timeout=timeout)
 
 if __name__ == "__main__":
     inp1 = input("Enter the Heading\n")
     inp2 = input("Enter the Message\n")
+    inp3 = input("Enter how many seconds the Notification should stay")
     Notification(inp1,inp2)    
